ed5f53f6ee4b3ab20bc49204f2bb7817ea6f9983
Priyansh <priyanshthummar0702@gmail.com>
2026-02-13 20:27:17 +0530

    first commit

---
+++ simple-chat/server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const bcrypt = require("bcrypt");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+// Fixed users with passwords
+const users = {
+    rahul: bcrypt.hashSync("1234", 10),
+    priya: bcrypt.hashSync("5678", 10)
+};
+
+let activeUsers = {};
+
+io.on("connection", (socket) => {
+
+    socket.on("login", async ({ username, password }) => {
+
+        if (!users[username]) {
+            return socket.emit("errorMsg", "User not allowed");
+        }
+
+        const valid = await bcrypt.compare(password, users[username]);
+
+        if (!valid) {
+            return socket.emit("errorMsg", "Wrong password");
+        }
+
+        if (Object.keys(activeUsers).length >= 2) {
+            return socket.emit("errorMsg", "Chat already in use");
+        }
+
+        activeUsers[username] = socket.id;
+        socket.username = username;
+
+        socket.emit("loginSuccess");
+    });
+
+    socket.on("message", (msg) => {
+        socket.broadcast.emit("message", {
+            user: socket.username,
+            text: msg
+        });
+    });
+
+    socket.on("disconnect", () => {
+        delete activeUsers[socket.username];
+    });
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log("Server running on port " + PORT);
+});
